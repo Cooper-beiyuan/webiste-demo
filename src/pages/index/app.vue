fb37df56762960460aedea5987dc82d799abb8ad
Cooper-beiyuan <cooper@drpanda.com>
2020-07-16 22:11:09 +0800

    init

---
+++ src/pages/index/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    home page
+    <div>{{`Title: ${title}`}}</div>
+    <div>{{`Subtitle: ${subtitle}`}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      title: '',
+      subtitle: '',
+    };
+  },
+  created: function() {
+    this.title = window.pageData.title;
+    this.subtitle = window.pageData.subtitle;
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 36px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,30 @@
+'use strict'
+const titles = require('./title.js')
+const templates = require('./templates.js')
+const glob = require('glob')
+const pages = {}
+
+glob.sync('./src/pages/**/app.js').forEach(path => {
+  const chunk = path.split('./src/pages/')[1].split('/app.js')[0]
+  console.log('path', path, chunk)
+  let filename = `${chunk}/index.html`;
+  if (chunk === 'index') {
+    filename = 'index.html';
+  }
+  pages[chunk] = {
+    entry: path,
+    template: templates[chunk] ? templates[chunk] : 'public/index.html',
+    title: titles[chunk] ? titles[chunk] : '熊猫博士',
+    data: chunk,
+    filename: filename,
+    chunks: ['chunk-vendors', 'chunk-common', chunk]
+  }
+})
+
+module.exports = {
+  publicPath: '/',
+  pages,
+  chainWebpack: config => {
+    config.plugins.delete('named-chunks')
+  },
+}
